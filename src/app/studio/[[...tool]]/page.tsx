249439311181ac5513a3bdc1c772c9400ee87f61
Tab & Tech <tabandtech@users.noreply.github.com>
2026-09-18 14:13:14 +0500

    Fix Studio: dynamic import with ssr:false to prevent hydration issues

--- src/app/studio/[[...tool]]/page.tsx
+++ src/app/studio/[[...tool]]/page.tsx
@@ -1,10 +1,13 @@
 'use client'
 
-import { NextStudio } from 'next-sanity/studio'
+import dynamic from 'next/dynamic'
 import config from '@/sanity/sanity.config'
 
-const StudioPage = () => {
+const NextStudio = dynamic(
+  () => import('next-sanity/studio').then((mod) => mod.NextStudio),
+  { ssr: false }
+)
+
+export default function StudioPage() {
   return <NextStudio config={config} />
 }
-
-export default StudioPage
